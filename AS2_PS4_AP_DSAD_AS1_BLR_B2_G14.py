b0dc1195cbb66874bc72f0da05f37a0e346139fe
Shiva <hmshivap@gmail.com>
2019-09-04 22:27:44 +0530

    Just the program without the comments

--- AS2_PS4_AP_DSAD_AS1_BLR_B2_G14.py
+++ AS2_PS4_AP_DSAD_AS1_BLR_B2_G14.py
@@ -1,9 +1,16 @@
 """
 Assignment program using Dynamic Programming bitmasking technique
+The program depends on caching the results of sub problems as part
+of dynamic programming and a bitmask to store the assignment of
+assignments to the student.
+Each bit in the mask representing such an assignment.
+The final goal is to find all combinations where we will end up with
+the bit mask with all bits set to 1's , which indicates that all the
+students have been assigned one of the assignments of their preference
 """
 
 # Input file name
-# Data seperated by '/'
+# Data separated by '/'
 # First item in the row is the student number and further are the topics selected for the students
 input_file_name = 'inputPS4.txt'
 
@@ -17,13 +24,16 @@ n = 11
 subjects = ['AI', 'BD', 'CC', 'DM', 'EC',
             'GM', 'IP', 'ML', 'NLP', 'SDA', 'WMC']
 
-# 2d matrix for preferences of topics
+# Preferences represented course wise
+# each row will show all the students preferred that particular course
 course_matrix = []
 
+# Initialize the preferences to 0
 preferences = [[0 for j in range(n)] for i in range(n)]
 
 # Maximum value - all the bits to one
 ones = (1 << n) - 1
+
 
 
 def insert_into_matrix(student_options, student):
@@ -50,10 +60,13 @@ def insert_into_matrix(student_options, student):
 
 def assignments(matrix, bitmask, assignment):
     """
-    Method for assignment with bitmasking technique
+    Method that implement the crux of dynamic programming
+    logic with bitmasks
     """
 
-    # Base condition for ones
+    # Base condition for ones, if all the bits are set
+    # then we found one combination of assignment
+    # So, return 1
     if bitmask == ones:
         return 1
 
@@ -62,6 +75,8 @@ def assignments(matrix, bitmask, assignment):
         return 0
 
     # Return if the value already computed by checking with -1
+    # Pick the value if it already available, making this code
+    # logic stay true to dynamic programming techniques
     if matrix[bitmask][assignment] != -1:
         return matrix[bitmask][assignment]
 
@@ -69,9 +84,12 @@ def assignments(matrix, bitmask, assignment):
     combinations = assignments(matrix, bitmask, assignment+1)
 
     for student in range(n):
+        # Check if this student preferred the topic
         if preferences[assignment-1][student] == 0:
             continue
 
+        # If the topic was preferred , then update
+        # the bit as 1 and continue
         if bitmask & (1 << student):
             continue
 
@@ -80,23 +98,27 @@ def assignments(matrix, bitmask, assignment):
                                     (1 << student), assignment + 1)
 
     # Update the combinations to the matrix
+    # This result from a sub problem will be reused in higher levels
     matrix[bitmask][assignment] = combinations
 
     return matrix[bitmask][assignment]
 
 
 def main():
+    # Read the input file and update the course preference matrix
     with open(input_file_name, 'r') as input_file:
         student = 0
         for option in input_file:
             # Sample format: S1 / DM / SDA / WMC / CC
             insert_into_matrix(option, student)
-        # print(course_matrix)
 
+    # Update the topic wise preferences
     for i in range(n):
         for j in range(n):
             preferences[j][i] = course_matrix[i][j]
 
+    # This the dynamic programming matrix to store all the values of
+    # problem and sub problems
     recursiveMatrix = [[-1 for j in range(n+1)] for i in range(2 ** n)]
 
     value = assignments(recursiveMatrix, 0, 1)
